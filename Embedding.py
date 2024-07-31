f1dc7a89e15a253286c650553c61092db19deaba
Somewhatmilk <94892240+Somewhatmilk@users.noreply.github.com>
2024-07-31 09:39:40 +0800

    Delete Embedding.py

--- Embedding.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoModel
-from numpy.linalg import norm
-
-
-token="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"
-cos_sim = lambda a,b: (a @ b.T) / (norm(a)*norm(b))
-model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-base-en',token=token, trust_remote_code=True) # trust_remote_code is needed to use the encode method
-# embeddings = model.encode(['How is the weather today?', 'What is the current weather like today?'])
-# print(cos_sim(embeddings[0], embeddings[1]))
